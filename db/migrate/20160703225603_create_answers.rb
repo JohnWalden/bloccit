fd68dad493d34883fe8aa58e3629c9775e76e58b
John Walden <jwwkc7@mst.edu>
2016-07-04 22:15:48 +0000

    Test for Quetion passes

--- db/migrate/20160703225603_create_answers.rb
+++ db/migrate/20160703225603_create_answers.rb
@@ -2,7 +2,7 @@ class CreateAnswers < ActiveRecord::Migration
   def change
     create_table :answers do |t|
       t.text :body
-      t.References :Question
+      t.references :Question
 
       t.timestamps null: false
     end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:question) {Question.create!(title: "New Post Title", body: "New Post Body", resoved: false) }
+  describe "attributes" do
+      it "has title and body attributes, and is not resolved" do
+          expect(question).to have_attributes(title: "New Post Title", body: "New Post Body", resoved: false)
+      end
+  end
 end
