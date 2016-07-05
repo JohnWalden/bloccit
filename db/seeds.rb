ff43242e0bd289084bd2749f5c5b800cd9464043
John Walden <jwwkc7@mst.edu>
2016-07-05 19:31:02 +0000

    fixeed errors in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,25 @@
-require 'random_data'
+ require 'random_data'
 
-50.time do
-    Post.create!(
-        title: RandomData.random_sentence,
-        body: RandomData.random_paragraph
-        )
-end
 
-posts = Post.all
+ 50.times do
 
-100.times do
-    Comment.create!(
-        post: posts.sample,
-        body: RandomData.random_paragraph
-        )
-    end
-    
-puts "Seed finished"
-puts "#{Posts.count} posts created"
-puts "#{Comment.count} Comments created"
+   Post.create!(
+
+     title:  RandomData.random_sentence,
+     body:   RandomData.random_paragraph
+   )
+ end
+ posts = Post.all
+ 
+
+ 100.times do
+   Comment.create!(
+
+     post: posts.sample,
+     body: RandomData.random_paragraph
+   )
+ end
+ 
+ puts "Seed finished"
+ puts "#{Post.count} posts created"
+ puts "#{Comment.count} comments created"
