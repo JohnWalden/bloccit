eba1d982b0aea1e60b1b0a3426d51fb02bb3fd59
John Walden <jwwkc7@mst.edu>
2016-07-05 04:11:31 +0000

    rspec for answer complete

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+ let(:question) {Question.create!(title: "New Post Title", body: "New Post Body", resoved: false) }
+ let(:answer) {Answer.create!(body: 'Comment Body')}
+ 
+ 
+describe "attributes" do
+      it "has a body attribute" do
+          expect(answer).to have_attributes(body: "Comment Body")
+      end
+  end
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Question, type: :model do
   let(:question) {Question.create!(title: "New Post Title", body: "New Post Body", resoved: false) }
+  
   describe "attributes" do
       it "has title and body attributes, and is not resolved" do
           expect(question).to have_attributes(title: "New Post Title", body: "New Post Body", resoved: false)
