d5f157165c1d325230dbe6987f49167363ee28d9
John Walden <jwwkc7@mst.edu>
2016-07-09 23:53:42 +0000

    I dont know

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,4 @@
 Rails.application.routes.draw do
-  get 'advertisements/index'
-
-  get 'advertisements/show'
-
-  get 'advertisements/new'
-
-  get 'advertisements/create'
-
-  get 'posts/index'
-
-  get 'posts/show'
-
-  get 'posts/new'
-
-  get 'posts/edit'
   
   resources :advertisements
 
