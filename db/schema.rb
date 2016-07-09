ee23aa2bb5f795d11b4ced0d1a96b3ef394a58d8
John Walden <jwwkc7@mst.edu>
2016-07-09 18:47:08 +0000

    created models, added data base, and forms rspec tests

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160703221128) do
+ActiveRecord::Schema.define(version: 20160709184357) do
+
+  create_table "advertisements", force: :cascade do |t|
+    t.text     "title"
+    t.text     "body"
+    t.integer  "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "advertisments", force: :cascade do |t|
+    t.string   "title"
+    t.text     "copy"
+    t.integer  "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
---
+++ spec/models/advertisment_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe Advertisment, type: :model do
+    let (:advertisement) { Advertisment.create!}
+    
+    describe "attributes: do"
+        it "Should repsond to title" do
+            expect(advertisement).to respond_to(:title)
+        end
+        
+        it "should respond to copy" do
+            expect(advertisement).to respond_to(:copy)
+        end
+        
+        it "should respond to price" do
+            expect(advertisement).to respond_to(:price)
+        end
+    end
+end
