02902eb5491845d3aab821eaa265799a2de43425
John Walden <jwwkc7@mst.edu>
2016-07-11 04:52:54 +0000

    updated rspec & filled in def edit in posts_controller.rb

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -31,36 +31,38 @@ RSpec.describe PostsController, type: :controller do
 #     end
 
   describe "GET edit" do
-    it "returns http success" do
-      get edit, {id: my_post.id}
-      expect(response).to have_http_status(:success)
-    end
-    
-    it "renders ethe #edit view" do
-      get :edit, {id: my_post.id}
-      expect(response).to render_template :edit
-    end
-    
-    it "assigns post to be updated to @post" do
-      get :edit, {id: my_post.id}
-      
-      post_instance = assigns(:post)
-      
-      expect(post_instance.id).to eq my_post.id
-      expect(post_instance.title).to eq my_post.title
-      expect(post_instance.body).to eq my_post.body
-    end
-    
-     it "renders the #show view" do
-       get :show, {id: my_post.id}
-       expect(response).to render_template :show
+     it "returns http success" do
+       get :edit, {id: my_post.id}
+       expect(response).to have_http_status(:success)
      end
  
-     it "assigns my_post to @post" do
-       get :show, {id: my_post.id}
-       expect(assigns(:post)).to eq(my_post)
+     it "renders the #edit view" do
+       get :edit, {id: my_post.id}
+       expect(response).to render_template :edit
+     end
+
+     it "assigns post to be updated to @post" do
+       get :edit, {id: my_post.id}
+ 
+       post_instance = assigns(:post)
+ 
+       expect(post_instance.id).to eq my_post.id
+       expect(post_instance.title).to eq my_post.title
+       expect(post_instance.body).to eq my_post.body
      end
    end
+    
+#     it "renders the #show view" do
+#       get :show, {id: my_post.id}
+#       expect(response).to render_template :show
+#     end
+ 
+#     it "assigns my_post to @post" do
+#      get :show, {id: my_post.id}
+#       expect(assigns(:post)).to eq(my_post)
+#     end
+   
+   
 
   #describe "GET #new" do
   #  it "returns http success" do
