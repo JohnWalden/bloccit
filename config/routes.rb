886e01880bb3e95a02daa063d23498eefb837de9
John Walden <jwwkc7@mst.edu>
2016-07-18 21:39:07 +0000

    Created controller for questions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  get 'question/index'
+
+  get 'question/new'
+
+  get 'question/create'
+
+  get 'question/show'
+
+  get 'question/edit'
+
+  get 'question/update'
+
+  get 'question/destroy'
+
   get 'posts/index'
 
   get 'posts/show'
