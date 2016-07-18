e0beb696f67473efd2e68f13ee86cacb6b7b8532
John Walden <jwwkc7@mst.edu>
2016-07-18 21:28:24 +0000

    Created odel for Question and Rspec tested it

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160703221128) do
+ActiveRecord::Schema.define(version: 20160718211958) do
+
+  create_table "adds", force: :cascade do |t|
+    t.string   "title"
+    t.text     "copy"
+    t.integer  "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "advertisements", force: :cascade do |t|
+    t.text     "title"
+    t.text     "body"
+    t.integer  "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "advertisments", force: :cascade do |t|
+    t.string   "title"
+    t.text     "copy"
+    t.integer  "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -25,6 +49,14 @@ ActiveRecord::Schema.define(version: 20160703221128) do
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "questions", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.boolean  "resolved"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
