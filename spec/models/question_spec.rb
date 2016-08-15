fd20781ae160fe391cffd2977d55d4f0cce66ee5
John Walden <jwwkc7@mst.edu>
2016-08-15 16:42:50 +0000

    busted

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+  context "attribute" do
+      let(:question) { Question.new(title: "New Question Title", body: "New Questionbody", resolved: false) }
+          
+          it "should respond to tittle" do
+            expect(question).to respond_to(:title)
+          end
+          
+          it "should respond to body" do
+            expect(question).to respond_to(:body)
+          end
+          
+          it "should respond to resolved"
+            expect(question).to respond_to(:body)
+          end
+      end
