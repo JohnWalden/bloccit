dbf580b78c53397cb9ba3acee7b70611f4efb5e3
John Walden <jwwkc7@mst.edu>
2016-08-15 17:21:48 +0000

    added tests for questions controllers

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -2,32 +2,24 @@ require 'rails_helper'
 
 RSpec.describe QuestionsController, type: :controller do
 
-  describe "GET #index" do
-    it "returns http success" do
-      get :index
-      expect(response).to have_http_status(:success)
+  let (:my_question) do
+      Question.create(
+          id: 1,
+          title: RandomData.random_sentence,
+          body: RandomData.random_paragraph,
+          resolved: false
+       )
+   end
+   
+   describe "GET #index" do
+       it "returns http success" do
+           get :index
+           expect(response).to have_http_status(:success)
+       end
+       
+       it "assigns my_question to @question" do
+           get :index
+           expect(assigns(:questions)).to eq([my_question])
+       end
     end
-  end
-
-  describe "GET #show" do
-    it "returns http success" do
-      get :show
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET #new" do
-    it "returns http success" do
-      get :new
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET #edit" do
-    it "returns http success" do
-      get :edit
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-end
+   end
