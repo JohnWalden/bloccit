330236e798f9e28069d7d50fc014d7c5fcc5fa89
John Walden <jwwkc7@mst.edu>
2016-09-29 20:31:57 +0000

    Added def new to topics controller

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -3,7 +3,11 @@ class TopicsController < ApplicationController
         @topics = Topic.all
     end
     
-    def show
-        @topic - Topic.find(params[:id])
-    end
+     def show
+        @topic = Topic.find(params[:id])
+     end
+     
+     def new 
+         @topic = Topic.new
+     end
 end
