77d8fcc9fb2b2c623633608c1bd3b05deafba690
John Walden <jwwkc7@mst.edu>
2016-10-03 20:14:59 +0000

    removed index from posts_controller.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :topics do
-    resources :posts, expect: [:index]
-  end
+     resources :posts, except: [:index]
+   end
 
   get 'about' => 'welcome#about'
   
